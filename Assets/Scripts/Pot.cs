fc349c1c4ffcb68857287515a47ca46362fad890
Wander <art.li.baum@gmail.com>
2022-11-19 20:16:22 -0800

    Pots

--- Assets/Scripts/Pot.cs
+++ Assets/Scripts/Pot.cs
@@ -1,26 +1,32 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Pot : MonoBehaviour
 {
-    [SerializeField]private BoxCollider2D _collider;
+    [SerializeField] private BoxCollider2D _collider;
+    [SerializeField, Range(0.5f, 1f)] private float delayTime;
     private PlayerMovement pm;
-    private bool breakable= false;
+    private Rigidbody2D body;
+    private bool breakable = false;
     public Sprite _breakSprite;
     public SpriteRenderer _Renderer;
     public GameObject key;
 
-
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(Falling());
-
+            if (!breakable)
+                StartCoroutine(Falling());
         }
     }
 
@@ -35,10 +41,14 @@ public class Pot : MonoBehaviour
     IEnumerator Falling()
     {
         _collider.enabled = false;
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(delayTime);
         _collider.enabled = true;
         breakable = true;
         _Renderer.sprite = _breakSprite;
         key.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        _Renderer.enabled = false;
+        body.isKinematic = true;
+        _collider.enabled = false;
     }
 }
