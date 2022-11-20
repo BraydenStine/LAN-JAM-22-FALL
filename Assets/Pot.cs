e193bbb7e40c9aea0fede79248843a07d293d7fd
Wander <art.li.baum@gmail.com>
2022-11-19 20:07:30 -0800

    added assets

--- Assets/Pot.cs
+++ Assets/Pot.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Pot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]private BoxCollider2D _collider;
+    private PlayerMovement pm;
+    private bool breakable= false;
+    public Sprite _breakSprite;
+    public SpriteRenderer _Renderer;
+    public GameObject key;
+
+
+
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if (col.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Falling());
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        
+        if (breakable)
+        {
+            //break
+        }
+    }
+
+    IEnumerator Falling()
+    {
+        _collider.enabled = false;
+        yield return new WaitForSeconds(1.5f);
+        _collider.enabled = true;
+        breakable = true;
+        _Renderer.sprite = _breakSprite;
+        key.SetActive(true);
     }
 }
