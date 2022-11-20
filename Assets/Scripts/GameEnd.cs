eba92f33dd3b2257ce8461ebbd6f8fa3aee0dbd2
Brayden Stine <79546261+BraydenStine@users.noreply.github.com>
2022-11-19 19:59:23 -0800

    gauhekj

--- Assets/Scripts/GameEnd.cs
+++ Assets/Scripts/GameEnd.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class GameEnd : MonoBehaviour
 {
-    public GameObject BEEZ;
-
-    public PlayerStats Health;
+    public GameObject Hand;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            BEEZ.SetActive(true);
-            Health.CurrentHealth = -1;
+            Hand.SetActive(true);
         }
     }
 }
