bead82ef0f0c2d7fbb13d68487cd17d6cc738707
Brayden Stine <79546261+BraydenStine@users.noreply.github.com>
2022-11-20 20:27:03 -0800

    Quit game

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -95,6 +95,10 @@ public class PlayerMovement : MonoBehaviour
         {
             Player.transform.position = RestartPos.transform.position;
         }
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     private void FixedUpdate()
